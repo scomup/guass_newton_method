a8dea683d7979eafd5dcea55bddda700dc1d0054
liu.yang <scomup@gmail.com>
2022-01-13 22:59:11 +0900

    t1

--- 3d_newton.py
+++ 3d_newton.py
@@ -28,48 +28,30 @@ def x2m(x):
     m[0:3,3] = t
     return m
 
-def calcdTdx():
-    A1 = np.array([0, 0, 0, 1,  0, 0, 0, 0,  0, 0, 0, 0. ])
-    A2 = np.array([0, 0, 0, 0,  0, 0, 0, 1,  0, 0, 0, 0. ])
-    A3 = np.array([0, 0, 0, 0,  0, 0, 0, 0,  0, 0, 0, 1. ])
-    A4 = np.array([0, 0, 0, 0,  0, 0,-1, 0,  0, 1, 0, 0. ])
-    A5 = np.array([0, 0, 1, 0,  0, 0, 0, 0, -1, 0, 0, 0. ])
-    A6 = np.array([0,-1, 0, 0,  1, 0, 0, 0,  0, 0, 0, 0. ])
-    dTdx = np.zeros([12,6])
-    dTdx[:,0] = A1
-    dTdx[:,1] = A2
-    dTdx[:,2] = A3
-    dTdx[:,3] = A4
-    dTdx[:,4] = A5
-    dTdx[:,5] = A6
-    return dTdx
-
-def calcdfdT(a):
-    dfdT = np.empty((0,12), float)
-    x = a[0]
-    y = a[1]
-    z = a[2]
-    tmp = np.array([[Tinit[0,0]*x, Tinit[0,0]*y, Tinit[0,0]*z, Tinit[0,0],  Tinit[0,1]*x, Tinit[0,1]*y, Tinit[0,1]*z, Tinit[0,1],  Tinit[0,2]*x, Tinit[0,2]*y, Tinit[0,2]*z, Tinit[0,2]],
-                    [Tinit[1,0]*x, Tinit[1,0]*y, Tinit[1,0]*z, Tinit[1,0],  Tinit[1,1]*x, Tinit[1,1]*y, Tinit[1,1]*z, Tinit[1,1],  Tinit[1,2]*x, Tinit[1,2]*y, Tinit[1,2]*z, Tinit[1,2]],
-                    [Tinit[2,0]*x, Tinit[2,0]*y, Tinit[2,0]*z, Tinit[2,0],  Tinit[2,1]*x, Tinit[2,1]*y, Tinit[2,1]*z, Tinit[2,1],  Tinit[2,2]*x, Tinit[2,2]*y, Tinit[2,2]*z, Tinit[2,2]]])
-    return tmp
-
-def trans_error(p, a, b):
-    dT = x2m(p)
-    T = Tinit.dot(dT)
+def trans_error(init_p, a, b):
+    T = x2m(init_p)
     return np.dot(T[0:3,0:3],a) + T[0:3,3] - b
 
 def ndt_cost(trans_error):
     return trans_error.T.dot(cov_inv.dot(trans_error))
 
-def fun(p, a, b):
-    x = trans_error(p, a, b)
+def fun(init_p, a, b):
+    x = trans_error(init_p, a, b)
     c = ndt_cost(x)
     f = -d1* np.exp(-d2 / 2 * c)
     return f
 
-def calcdxdp(a):
-    dxdp = calcdfdT(a).dot(calcdTdx())
+def calcdxdp(init_p, a):
+    T = x2m(init_p)
+    x = a[0]
+    y = a[1]
+    z = a[2] 
+    dxdp = np.array([[T[0,0], T[0,1], T[0,2], -T[0,1]*z + T[0,2]*y, T[0,0]*z - T[0,2]*x, -T[0,0]*y + T[0,1]*x],
+                    [T[1,0], T[1,1], T[1,2], -T[1,1]*z + T[1,2]*y, T[1,0]*z - T[1,2]*x, -T[1,0]*y + T[1,1]*x],
+                    [T[2,0], T[2,1], T[2,2], -T[2,1]*z + T[2,2]*y, T[2,0]*z - T[2,2]*x, -T[2,0]*y + T[2,1]*x],])
+
+
+
     return dxdp
 
 def calcdx2dp2(a):
@@ -89,13 +71,13 @@ def calcdx2dp2(a):
     return dx2dp2
 
 
-def gradient_hessian(p, a, b):
-    f = fun(p, a, b)
-    x = trans_error(p, a, b)
-    dxdp = calcdxdp(a)
+def gradient_hessian(init_p, a, b):
+    f = fun(init_p, a, b)
+    x = trans_error(init_p, a, b)
+    dxdp = calcdxdp(init_p, a)
     dx2dp2 = calcdx2dp2(a)
     g = f *(-d2 * x.T.dot(cov_inv.dot(dxdp)))
-    s = p.shape[0]
+    s = init_p.shape[0]
     h = np.zeros([s, s])
     x_cinv_dxdp = x.dot(cov_inv.dot(dxdp))
     for i in range(s):
@@ -104,22 +86,17 @@ def gradient_hessian(p, a, b):
             h[i,j] = f * (-d2 )*  (-d2*x_cinv_dxdp[i]*x_cinv_dxdp[j]  +x_cinv_dx2dpij +  dxdp[:,j].dot(cov_inv.dot(dxdp[:,i])))
     return g, h
 
-p = np.array([0,0,0,0,0,0])
-cur_p = p
-
 """
 Generating test data
 """
 elements = 100
 A =  (np.random.rand(elements,3)-0.5)*2
 
-#B = transform3d(np.array([0.2,0.1,-0.2, 0.5,-1.5,0.1]), A.T).T
 B = transform3d(np.array([0.2,0.1,-0.2, 0.3,0.3,0.3]), A.T).T
-init_x = np.array([1000,1000,1000, 0.6,0.6,1.2])
-#A = transform3d(init_x, A.T).T
-B = transform3d(init_x, B.T).T
-Tinit = x2m(init_x)
-T = Tinit.dot(np.eye(4))
+init_p = np.array([1000,1000,1000, 0.6,0.6,1.2])
+B = transform3d(init_p, B.T).T
+#Tinit = x2m(init_p)
+#T = Tinit.dot(np.eye(4))
 fig = plt.figure()
 ax = Axes3D(fig)
 ax = Axes3D(fig)
@@ -131,23 +108,23 @@ ax.set_zlabel("Z")
 cur_A = A
 while(True):
     plt.cla()
-    Ashow = transform3d(init_x, cur_A.T).T
+    Ashow = transform3d(init_p, cur_A.T).T
     ax.scatter3D(Ashow[:,0],Ashow[:,1],Ashow[:,2], c= 'r')
     ax.scatter3D(B[:,0],B[:,1],B[:,2], c= 'b')
     plt.pause(0.1)
 
 
-    s = p.shape[0]
+    s = init_p.shape[0]
     h = np.zeros([s,s])
     g = np.zeros(s)
     score = 0
     for i in range(cur_A.shape[0]):
         a = cur_A[i]
         b = B[i]
-        point_g, point_h = gradient_hessian(p, a, b)
+        point_g, point_h = gradient_hessian(init_p, a, b)
         h = h + point_h
         g = g + point_g
-        score = score - fun(p, a, b)
+        score = score - fun(init_p, a, b)
     print(score)
     dp = np.linalg.pinv(-h).dot(g)
     if(np.linalg.norm(dp) < 0.0001):
