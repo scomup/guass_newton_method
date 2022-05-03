21d6e6a005ca0854dd37becd0991125a7bc5b07e
liu.yang <scomup@gmail.com>
2022-05-03 17:02:18 +0900

    2

--- robustkernel.py
+++ robustkernel.py
@@ -1,10 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
+# Christopher Zach. “Robust bundle adjustment revisited” (11)(12)
 
 def L2Kernel(e2):
     rho = [None,None,None]
     rho[0] = e2
     rho[1] = 1.
+    rho[2] = 0
+    return rho
+
+def L1Kernel(e2):
+    rho = [None,None,None]
+    rho[0] = np.sqrt(e2)
+    rho[1] = 1/(2*np.sqrt(e2))
     rho[2] = 0
     return rho
 
@@ -53,6 +61,7 @@ def solveR(a, b, kernel):
     J[0,:] = a
     J[1,:] = 1.
     J = J.T
+    cont = 0
     while(True):
         cost = 0.
         error = a*x[0] + x[1] - b
@@ -62,14 +71,19 @@ def solveR(a, b, kernel):
             error2 = error[i]*error[i]
             rho = kernel(error2)
             j = J[i].reshape(1,2)
-            w = rho[1] # 2 * rho[2] * error2
+            w = rho[1]  #+ 2*rho[2] * error2
+            #n = np.linalg.norm(w)
+            #print(n)
             H += j.T.dot(j * w)
             g += j.T.dot(error[i] * rho[1])
             cost += rho[0]
+        cont += 1
         dx = np.linalg.solve(H, -g)
+        #print(dx)
         x = x + dx
         if(np.linalg.norm(dx)<0.00001):
             break
+    #print(cont)
     return x
 
 
@@ -78,9 +92,11 @@ def drawKernel():
     e2 = e**2
     eHuber = PseudoHuberKernel(e2)
     eCauchy = CauchyKernel(e2)
+    eL1 = L1Kernel(e2)
     plt.plot(e, eHuber[0], label='HuberKernel')
     plt.plot(e, eCauchy[0], label='CauchyKernel')
     plt.plot(e, e2, label='L2')
+    plt.plot(e, eL1[0], label='L1')
     plt.legend()
     plt.show()
 
@@ -96,12 +112,14 @@ b[13] = 6
 x0 = solveR(a, b, L2Kernel)
 x1 = solveR(a, b, PseudoHuberKernel)
 x2 = solveR(a, b, CauchyKernel)
+x3 = solveR(a, b, L1Kernel)
 
 
 plt.scatter(a,b)
 plt.plot(a,a*x0[0] + x0[1],label='L2')
 plt.plot(a,a*x1[0] + x1[1],label='HuberKernel')
 plt.plot(a,a*x2[0] + x2[1],label='CauchyKernel')
+plt.plot(a,a*x3[0] + x3[1],label='L1')
 plt.legend()
 
 
